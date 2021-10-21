4910ed57b4c01b9cfff316433858a88dabc66dbc
prashansa-shukla <prashufreelance@gmail.com>
2021-10-22 01:27:35 +0530

    home page icon

--- src/components/AppBar/appBar.js
+++ src/components/AppBar/appBar.js
@@ -5,25 +5,29 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
-//import MenuIcon from "@material-ui/icons";
+import { Link } from "react-router-dom";
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
 
 export default function MyAppBar() {
   return (
     <Box sx={{ flexGrow: 1, marginBottom: "40px" }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            {/* <MenuIcon /> */}
-          </IconButton>
+          <Link to="/">
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <AdminPanelSettingsIcon />
+            </IconButton>
+          </Link>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Generate SSL certificate
           </Typography>
+
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
